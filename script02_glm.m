a092e418800a88734e341d4d5ceab066e4d60aab
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2017-10-31 18:32:12 -0700

    add single run functionality

--- script02_glm.m
+++ script02_glm.m
@@ -102,58 +102,108 @@ for s = 1:length(subDirs)
     else
     end
     opt.wantparametric = true;
-    [glm_results,denoised_data] = GLMdenoisedata(glm_design{s},glm_data,stimDur,trDur,'assume',[],opt,figPath);
+    [glm_results,denoised_data] = GLMdenoisedata(glm_design{s},glm_data,stimDur,stimDur,'assume',[],opt,figPath);
     save(matPath,'-struct','glm_results','-v7.3');
     save(matPath,'-append','denoised_data');
     clear glm_data;
 end
 
+%% INDIVIDUAL RUN GLMs
+% For demonstration purposes, we will adopt the strategy of assuming a fixed HRF
+% in the analysis of each run.  Here we compute a canonical HRF.
+hrf = getcanonicalhrf(stimDur,trDur)';
+
+for s = 1:length(subDirs)
+    dataFiles = subfiles(sprintf('%s/run*vr.nii.gz',subDirs{s}),1);    
+    for z=1:length(dataFiles)
+        outName = sprintf('glmIndividual_run%0.0f',z);
+        outPath = sprintf('%s/%s',subDirs{s},outName);
+        matPath = sprintf('%s.mat',outPath);
+        if exist(matPath,'file')
+            if ~overWrite
+                % if file exists, skip subject 
+                fprintf('skipping glm run %0.0f: %s\n',z,subDirs{s});
+                continue;
+            else
+            end
+        else
+            fprintf('running glm run %0.0f: %s\n',z,subDirs{s});
+        end
+        glm_struct = NIfTI.Read(dataFiles{z});
+        % subtract TRs
+        numTR = size(glm_struct.data,4);
+        glm_data = glm_struct.data(:,:,:,subTractTR+1:min(numTR,maxTR));
+        if any(isnan(glm_data(:)))
+            error('glm data cannot have NaNs!');
+        else
+        end
+        clear glm_struct;
+        glm_results = GLMestimatemodel(glm_design{s}(z),glm_data,stimDur,stimDur,'assume',hrf,0);
+        save(matPath,'-struct','glm_results','-v7.3');
+        clear glm_data;
+    end
+end
+
+
 %% CREATE NIFTIS
 for s = 1:length(subDirs)
-    outName = sprintf('%s/glmDenoise',subDirs{s});
-    niiPath = sprintf('%s.nii.gz',outName);
-    matPath = sprintf('%s.mat',outName);
     dataFiles = subfiles(sprintf('%s/run*vr.nii.gz',subDirs{s}),1);
     glm_struct = NIfTI.Read(dataFiles{1});
-    outStruct.hdr = glm_struct.hdr;
-    outStruct.method = glm_struct.method;
-    glm_results = load(matPath,'model*','R2','SNR');
-    betas = glm_results.modelmd{2};
-    snr = betas./glm_results.modelse{2}; % divide by voxel-wise standard error to get SNR
-    R2 = glm_results.R2./100; % variance explained, divide by 100
-    tempData(:,:,:,1:2:size(betas,4)*2) = betas;
-    tempData(:,:,:,2:2:size(betas,4)*2) = snr;
-    outStruct.data = cat(4,tempData,R2,glm_results.SNR); % betas + beta SNR, R2 and overall SNR
-    outStruct.hdr.dim(5) = size(outStruct.data,4);
-    if exist(niiPath,'file')
-        system(sprintf('rm %s',niiPath));
-    else
+    for z=1:(length(dataFiles)+1)
+        outStruct.hdr = glm_struct.hdr;
+        outStruct.method = glm_struct.method;
+        if z > length(dataFiles) 
+            % do all runs
+            outName = 'glmDenoise';
+        else
+            % do individual runs
+            outName = sprintf('glmIndividual_run%0.0f',z);
+        end
+        outPath = sprintf('%s/%s',subDirs{s},outName);
+        niiPath = sprintf('%s.nii.gz',outPath);
+        matPath = sprintf('%s.mat',outPath);
+        glm_results = load(matPath,'model*','R2','SNR');
+       
+        betas = glm_results.modelmd{2};
+        snr = betas./glm_results.modelse{2}; % divide by voxel-wise standard error to get SNR
+        R2 = glm_results.R2./100; % variance explained, divide by 100
+        tempData(:,:,:,1:2:size(betas,4)*2) = betas;
+        tempData(:,:,:,2:2:size(betas,4)*2) = snr;
+        outStruct.data = cat(4,tempData,R2,glm_results.SNR); % betas + beta SNR, R2 and overall SNR
+        outStruct.hdr.dim(5) = size(outStruct.data,4);
+        if exist(niiPath,'file')
+            system(sprintf('rm %s',niiPath));
+        else
+        end
+        NIfTI.Write(outStruct,niiPath);
+        % assign new labels to sub-bricks
+        afniCommand{1} = sprintf(...
+            'source ~/.bashrc; cd %s; 3drefit -fbuc -redo_bstat -relabel_all_str ''beta0 snr0 beta90 snr90 beta180 snr180 beta270 snr270 modelR2 modelSNR'' %s',subDirs{s}, niiPath);
+        % go from volume to surface
+        tempSub = split(subDirs{s},'_');
+        subID = tempSub{end};
+        if strcmp(subID,'nl-0034');
+            subID = 'skeri0004';
+        else
+        end
+        afniCommand{2} = sprintf(...
+            'source ~/.bashrc; cd %s; rm *h.%s.niml.dset; mriVol2Surf.py %s %s.nii.gz --surfvol %s_fs4_SurfVol_ns_Alnd_Exp+orig',subDirs{s},outName,subID,outName,subID);
+        status = system(afniCommand{1});
+        if status == 127
+            error('afni 3drefit command failed!');
+        else
+        end
+        if z > length(dataFiles) 
+            status = system(afniCommand{2});
+            if status == 127
+                error('afni mriVol2Surf command failed!');
+            else
+            end
+        else
+        end
+        clear tempData;
+        clear outStruct;
     end
-    NIfTI.Write(outStruct,niiPath);
-    % assign new labels to sub-bricks
-    afniCommand{1} = sprintf(...
-        '3drefit -fbuc -redo_bstat -relabel_all_str ''beta0 snr0 beta90 snr90 beta180 snr180 beta270 snr270 modelR2 modelSNR'' %s',niiPath);
-    % go from volume to surface
-    tempSub = split(subDirs{s},'_');
-    subID = tempSub{end};
-    if strcmp(subID,'nl-0034');
-        subID = 'skeri0004';
-    else
-    end
-    afniCommand{2} = sprintf(...
-        'source ~/.bashrc; cd %s; rm *h.glmDenoise.niml.dset; mriVol2Surf.py %s glmDenoise.nii.gz --surfvol %s_fs4_SurfVol_ns_Alnd_Exp+orig',subDirs{s},subID,subID);
-    status = system(afniCommand{1});
-    if status == 127
-        error('afni 3drefit command failed!');
-    else
-    end
-    status = system(afniCommand{2});
-    if status == 127
-        error('afni mriVol2Surf command failed!');
-    else
-    end
-    clear tempData;
-    clear outStruct;
 end
 
 %% DO STATS
