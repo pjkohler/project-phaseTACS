5451339ca339bbb1e58c17a56ffe5bb5bd3334e3
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2017-11-02 18:17:03 -0700

    classification added

--- script05_roiStats.m
+++ script05_roiStats.m
@@ -6,6 +6,7 @@ addpath(genpath([codeFolder,'/git/schlegel/matlab_lib']));
 addpath(genpath([codeFolder,'/git/mrC']));
 addpath(genpath([codeFolder,'/git/MRI/matlab']));
 
+
 wangROIs = {'V1','V2' 'V3','hV4' 'VO1' 'VO2' 'PHC1' 'PHC2' ...
             'TO1' 'LO2' 'LO1' 'V3B' 'V3A' 'IPS0' 'IPS1' 'IPS2' 'IPS3' 'SPL1' };
         % note truncated version with only 18 ROIs
@@ -42,31 +43,53 @@ end
 
 roiMean = cellfun(@(x) nanmean(x), roiData,'uni',false);
 
-save('tacsROIs.mat');
-
 %% MAKE FIGURES
 close all;
 dataIdx = 1:2:8;
+colors = {'-b','-r'};
 fontSize = 12;
 condLabels = {'0','90','180','270'};
-gcaOpts = {'tickdir','out','box','off','fontsize',fontSize,'fontname','Helvetica','linewidth',1};
-for s=3%1:length(SUB_DIR);
+gcaOpts = {'tickdir','out','box','off','fontsize',fontSize,'fontname','Helvetica','linewidth',1,'ticklength',[.03,.03]};
+roisToPlot = 1
+for s=1:length(SUB_DIR);
     figure;
-    for r = 1:3
-        subplot(1,3,r)
-        hold on
-        pH(1) = plot(1:4,roiMean{r,1,1,s}(:,dataIdx),'-b','linewidth',2);
-        arrayfun(@(x) plot(1:4,roiMean{r,1,x,s}(:,dataIdx),'-b','linewidth',1),2:numRuns(s)+1);
-        hold on;
-        pH(2) = plot(1:4,roiMean{r,2,1,s}(:,dataIdx),'-r','linewidth',2);
-        arrayfun(@(x) plot(1:4,roiMean{r,2,x,s}(:,dataIdx),'-r','linewidth',1),2:numRuns(s)+1);
-        title(sprintf('%s: %s %s',subNames{s},wangROIs{r},montageObj(sesNames{s})),'fontsize',12,'fontname','helvetica');
-        xlim([0.5,4.5]);
+    for r = 1:roisToPlot
+        for h = 1:2
+            subplot(roisToPlot,2,h+(r-1)*2);
+            hold on
+            pH(h) = plot(1:4,roiMean{r,h,1,s}(:,dataIdx),colors{h},'linewidth',2);
+            arrayfun(@(x) plot(1:4,roiMean{r,h,x,s}(:,dataIdx),colors{h},'linewidth',1),2:numRuns(s)+1);
+            hold off;
+            title(sprintf('%s: %s %s',subNames{s},wangROIs{r},montageObj(sesNames{s})),'fontsize',fontSize,'fontname','helvetica');
+            xlim([0.5,4.5]);
+            set(gca,gcaOpts{:},'xtick',1:4,'xticklabel',condLabels);
+        end
         %ylim([0,2]);
-        if r == 1
-            legend(pH(1:2),{'left','right'},'location','southwest');
+        if r == 3
+            legend(pH(1:2),{'left','right'},'location','southwest','box','off');
         else
         end
-        set(gca,gcaOpts{:},'xtick',1:4,'xticklabel',condLabels);
     end
 end
+
+%% CLASSIFICATION
+% prepare data
+leftData = [];
+rightData = [];
+targetArray = [];
+chunkArray = [];
+
+s = 6;
+r = 1;
+
+for z=1:numRuns(s)
+    tmpData = roiData{r,1,z+1,s}(:,dataIdx);
+    leftData = [leftData; permute(tmpData,[2,1])];
+    tmpData = roiData{r,2,z+1,s}(:,dataIdx);
+    rightData = [rightData; permute(tmpData,[2,1])];
+    targetArray = [targetArray; (1:4)'];
+    chunkArray = [chunkArray; ones(4,1)*z];
+end
+
+clfData = double(cat(2,leftData,rightData));
+res = MVPA.CrossValidation(clfData,targetArray,chunkArray,'zscore','data')
